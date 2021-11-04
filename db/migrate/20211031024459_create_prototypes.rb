3a96a6a9f8725ff8e8dea7143318eeb40eb91b55
yoshi0805 <90741509+yoshi0805@users.noreply.github.com>
2021-11-04 14:51:42 +0900

    Update 20211031024459_create_prototypes.rb

--- db/migrate/20211031024459_create_prototypes.rb
+++ db/migrate/20211031024459_create_prototypes.rb
@@ -1,7 +1,7 @@
 class CreatePrototypes < ActiveRecord::Migration[6.0]
   def change
     create_table :prototypes do |t|
-
+      
       t.timestamps 
       t.string :title,    null: false
       t.text :catch_copy, null: false
